908fecf9bbf12a71c45ae7cf95fe66690a3d6662
Ceri Westcott <ceriw@backbase.com>
2022-10-17 19:17:50 +0100

    Created working UI lib in nx

---
+++ libs/ui/src/lib/ui.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule, FlexModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const imports = [
+  FlexModule,
+  FlexLayoutModule,
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatProgressSpinnerModule,
+  MatMenuModule,
+  MatTableModule,
+  MatSelectModule,
+];
+
+@NgModule({
+  imports: [CommonModule, ...imports],
+  exports: [...imports],
+})
+export class UiModule {}
